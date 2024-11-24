6babcd53d001c29d26c1a30b328e2f2135ab52a7
prajwolkarki <prajwolkarki609@gmail.com>
2024-11-25 01:51:39 +0545

    added local upload folder to store the documents

--- src/middleware.ts
+++ src/middleware.ts
@@ -19,5 +19,5 @@ export function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/", "/login", "/profile/:path*", "/signup"],
+  matcher: [ "/login", "/profile/:path*", "/signup"],
 };
